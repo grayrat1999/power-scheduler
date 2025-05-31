49df61fb32953b92319b2f4c78c65a1de23d42f1
grayrat <grayrat1999@163.com>
2025-05-31 13:34:13 +0800

    feat(server): 新增CRON表达式验证接口

---
+++ power-scheduler-server/power-scheduler-server-application/src/main/kotlin/org/grayrat/powerscheduler/server/application/dto/request/CronParseRequestDTO.kt
@@ -0,0 +1,19 @@
+package org.grayrat.powerscheduler.server.application.dto.request
+
+/**
+ * Cron表达式解析接口
+ *
+ * @author grayrat
+ * @since 2025/4/16
+ */
+class CronParseRequestDTO {
+    /**
+     * Cron表达式
+     */
+    var cronExpression: String? = null
+
+    /**
+     * 下N次执行时间
+     */
+    var times: Int = 5
+}
---
+++ power-scheduler-server/power-scheduler-server-application/src/main/kotlin/org/grayrat/powerscheduler/server/application/service/ToolService.kt
@@ -0,0 +1,30 @@
+package org.grayrat.powerscheduler.server.application.service
+
+import org.grayrat.powerscheduler.server.application.dto.request.CronParseRequestDTO
+import org.grayrat.powerscheduler.server.application.exception.BizException
+import org.grayrat.powerscheduler.server.domain.utils.CronUtils
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+/**
+ * @author grayrat
+ * @since 2025/5/30
+ */
+@Service
+class ToolService {
+
+    fun parseCron(param: CronParseRequestDTO): List<LocalDateTime> {
+        val times = param.times
+        val cronExpression = param.cronExpression.orEmpty()
+        if (CronUtils.isValidCron(cronExpression).not()) {
+            throw BizException("Invalid Cron expression: $cronExpression")
+        }
+        return generateSequence(LocalDateTime.now()) {
+            CronUtils.nextExecution(
+                cronExpression = cronExpression,
+                baseTime = it
+            )
+        }.drop(1).take(times).toList()
+    }
+
+}
