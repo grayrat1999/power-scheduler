86c32856c509cc4c7bf984c41194c933838a7b07
grayrat <grayrat1999@163.com>
2025-05-31 14:59:27 +0800

    feat: 新增docker镜像构建脚本

---
+++ script/power-scheduler-server-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t power-scheduler-server -f ../power-scheduler-server/Dockerfile ../
---
+++ script/power-scheduler-worker-sample-springboot-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker build \
+  -t power-scheduler-worker-sample-springboot \
+  -f ../power-scheduler-worker-sample/power-scheduler-worker-sample-springboot/Dockerfile ../
