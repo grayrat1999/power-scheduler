d87aa5fff7f3222ff357373c496c4c46caf3d300
grayrat <grayrat1999@163.com>
2025-05-31 11:39:28 +0800

    feat(worker): 封装任务类, 实现任务执行流程和生命周期管理

---
+++ power-scheduler-worker/src/main/kotlin/org/grayrat/powerscheduler/worker/util/BasicThreadFactory.kt
@@ -0,0 +1,24 @@
+package org.grayrat.powerscheduler.worker.util
+
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * @author grayrat
+ * @since 2025/4/26
+ */
+class BasicThreadFactory(
+    private val threadNamePrefix: String? = null
+) : ThreadFactory {
+
+    override fun newThread(runnable: Runnable): Thread {
+        val thread = Thread(runnable, threadNamePrefix + THREAD_NUMBER.getAndIncrement())
+        thread.setDaemon(false)
+        thread.setPriority(Thread.NORM_PRIORITY)
+        return thread
+    }
+
+    companion object {
+        private val THREAD_NUMBER = AtomicInteger(1)
+    }
+}
