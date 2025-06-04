11924d146fac86c684b59d829536679281ca7a94
grayrat <grayrat1999@163.com>
2025-06-04 12:14:18 +0800

    feat: 试用版本限制部分功能的使用

---
+++ power-scheduler-worker-sample/power-scheduler-worker-sample-springboot/src/main/java/tech/powerscheduler/worker/sample/springboot/processor/ShortTimeJobProcessor.java
@@ -0,0 +1,29 @@
+package tech.powerscheduler.worker.sample.springboot.processor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import tech.powerscheduler.worker.job.JobContext;
+import tech.powerscheduler.worker.processor.JavaProcessor;
+import tech.powerscheduler.worker.processor.ProcessResult;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 短时间的任务
+ *
+ * @author grayrat
+ * @since 2025/6/3
+ */
+@Slf4j
+@Component
+public class ShortTimeJobProcessor extends JavaProcessor {
+
+    @Override
+    public ProcessResult process(JobContext context) throws InterruptedException {
+        log.info("job start");
+        TimeUnit.SECONDS.sleep(3);
+        log.info("job complete");
+        return ProcessResult.success();
+    }
+
+}
