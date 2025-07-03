7525104f6a138771ddedd13c60ec81b61c233143
grayrat <grayrat1999@163.com>
2025-07-03 10:26:15 +0800

    feat: 支持工作流

--- power-scheduler-server/power-scheduler-server-application/src/main/kotlin/tech/powerscheduler/server/application/assembler/WorkflowNodeAssembler.kt
+++ power-scheduler-server/power-scheduler-server-application/src/main/kotlin/tech/powerscheduler/server/application/assembler/WorkflowNodeAssembler.kt
@@ -1,11 +1,6 @@
 package tech.powerscheduler.server.application.assembler
 
 import org.springframework.stereotype.Component
-import tech.powerscheduler.server.application.dto.request.WorkflowNodeAddRequestDTO
-import tech.powerscheduler.server.application.dto.request.WorkflowNodeEditRequestDTO
-import tech.powerscheduler.server.application.dto.response.WorkflowNodeQueryResponseDTO
-import tech.powerscheduler.server.domain.workflow.Workflow
-import tech.powerscheduler.server.domain.workflow.WorkflowNode
 
 /**
  * @author grayrat
@@ -14,52 +9,5 @@ import tech.powerscheduler.server.domain.workflow.WorkflowNode
 @Component
 class WorkflowNodeAssembler {
 
-    fun toWorkflowNodeQueryResponseDTO(workflowNode: WorkflowNode): WorkflowNodeQueryResponseDTO {
-        return WorkflowNodeQueryResponseDTO().apply {
 
-        }
-    }
-
-    fun toDomainModel4AddRequest(workflow: Workflow, param: WorkflowNodeAddRequestDTO): WorkflowNode {
-        return WorkflowNode().apply {
-            this.workflow = workflow
-            this.parents = emptySet()
-            this.children = emptySet()
-            this.jobName = param.jobName
-            this.jobDesc = param.jobDesc
-            this.jobType = param.jobType
-            this.processor = param.processor
-            this.executeMode = param.executeMode
-            this.executeParams = param.executeParams
-            this.scriptType = param.scriptType
-            this.scriptCode = param.scriptCode
-            this.maxAttemptCnt = param.maxAttemptCnt
-            this.attemptInterval = param.attemptInterval
-            this.taskMaxAttemptCnt = param.taskMaxAttemptCnt
-            this.taskAttemptInterval = param.taskAttemptInterval
-            this.priority = param.priority
-        }
-    }
-
-    fun toDomainModel4EditRequest(workflowNode: WorkflowNode, param: WorkflowNodeEditRequestDTO): WorkflowNode {
-        return WorkflowNode().apply {
-            this.workflow = workflowNode.workflow
-            this.parents = workflowNode.parents
-            this.children = workflowNode.children
-            this.id = workflowNode.id
-            this.jobName = param.jobName
-            this.jobDesc = param.jobDesc
-            this.jobType = param.jobType
-            this.processor = param.processor
-            this.executeMode = param.executeMode
-            this.executeParams = param.executeParams
-            this.scriptType = param.scriptType
-            this.scriptCode = param.scriptCode
-            this.maxAttemptCnt = param.maxAttemptCnt
-            this.attemptInterval = param.attemptInterval
-            this.taskMaxAttemptCnt = param.taskMaxAttemptCnt
-            this.taskAttemptInterval = param.taskAttemptInterval
-            this.priority = param.priority
-        }
-    }
 }
--- power-scheduler-server/power-scheduler-server-application/src/main/kotlin/tech/powerscheduler/server/application/service/WorkflowNodeService.kt
+++ power-scheduler-server/power-scheduler-server-application/src/main/kotlin/tech/powerscheduler/server/application/service/WorkflowNodeService.kt
@@ -1,13 +1,7 @@
 package tech.powerscheduler.server.application.service
 
 import org.springframework.stereotype.Service
-import tech.powerscheduler.common.exception.BizException
 import tech.powerscheduler.server.application.assembler.WorkflowNodeAssembler
-import tech.powerscheduler.server.application.dto.request.WorkflowNodeAddRequestDTO
-import tech.powerscheduler.server.application.dto.request.WorkflowNodeEditRequestDTO
-import tech.powerscheduler.server.application.dto.response.WorkflowNodeQueryResponseDTO
-import tech.powerscheduler.server.domain.workflow.WorkflowId
-import tech.powerscheduler.server.domain.workflow.WorkflowNodeId
 import tech.powerscheduler.server.domain.workflow.WorkflowNodeRepository
 import tech.powerscheduler.server.domain.workflow.WorkflowRepository
 
@@ -22,30 +16,5 @@ class WorkflowNodeService(
     private val workflowNodeRepository: WorkflowNodeRepository,
 ) {
 
-    fun list(workflowId: Long): List<WorkflowNodeQueryResponseDTO> {
-        val workflow = workflowRepository.findById(WorkflowId(workflowId))
-        if (workflow == null) {
-            return emptyList()
-        }
-        val workflowNodes = workflowNodeRepository.findAllByWorkflow(workflow)
-        return workflowNodes.map { workflowNodeAssembler.toWorkflowNodeQueryResponseDTO(it) }
-    }
-
-    fun add(param: WorkflowNodeAddRequestDTO): Long {
-        val workflowId = WorkflowId(param.workflowId!!)
-        val workflow = workflowRepository.findById(workflowId)
-            ?: throw BizException("Workflow not found")
-        val workflowNode = workflowNodeAssembler.toDomainModel4AddRequest(workflow, param)
-        val workflowNodeId = workflowNodeRepository.save(workflowNode)
-        return workflowNodeId.value
-    }
-
-    fun edit(param: WorkflowNodeEditRequestDTO) {
-        val workflowNodeId = WorkflowNodeId(param.workflowNodeId!!)
-        val workflowNode = workflowNodeRepository.findById(workflowNodeId)
-            ?: throw BizException("workflowNode not found")
-        val workflowNodeToSave = workflowNodeAssembler.toDomainModel4EditRequest(workflowNode, param)
-        workflowNodeRepository.save(workflowNodeToSave)
-    }
 
 }
